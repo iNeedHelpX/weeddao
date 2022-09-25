83eee66c18192bdf8c610c989f51a361edde1931
Julia Pak <jpak0444@gmail.com>
2022-09-25 06:56:25 -0400

    globsl;

--- lib/globalvars.dart
+++ lib/globalvars.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,5 +11,3 @@ FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 FirebaseAuth auth = FirebaseAuth.instance;
 
 Logger logger = Logger();
-
-enum WeightUnits { Lb, Oz, Kg }
