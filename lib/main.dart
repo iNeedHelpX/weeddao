ac184fbb1926ef1174bef60a75a22d31d7d7f154
Julia Pak <jpak0444@gmail.com>
2022-09-25 07:00:17 -0400

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weeddao/Controllers/auth_controller.dart';
 import 'package:weeddao/Start/start.dart';
 import 'package:weeddao/globalvars.dart';
 
@@ -6,6 +8,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initialization.then((value) {
 //place controller access in here
+    Get.put(AuthController());
   });
   runApp(Start());
 }
