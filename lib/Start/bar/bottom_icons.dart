dfe130a8c287e14b0521ae16bbd57ce9b0a5a9c0
Julia Pak <jpak0444@gmail.com>
2022-09-25 02:10:13 -0400

    icons

--- lib/Start/bar/bottom_icons.dart
+++ lib/Start/bar/bottom_icons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:icofont_flutter/icofont_flutter.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:rolling_bottom_bar/rolling_bottom_bar_item.dart';
 import 'package:weeddao/icons/resized_joint.dart';
@@ -6,8 +7,8 @@ import 'package:weeddao/icons/resized_sci.dart';
 
 List<RollingBottomBarItem> get bottomBarItems {
   return const [
-    RollingBottomBarItem(JointMJIcon(),
-        label: 'About', activeColor: Colors.black),
+    RollingBottomBarItem(IcoFontIcons.weed,
+        label: 'Weed', activeColor: Colors.black),
     RollingBottomBarItem(MdiIcons.spiderThread,
         label: 'Spiders', activeColor: Colors.black),
     RollingBottomBarItem(MdiIcons.rodent,
