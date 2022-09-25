647f5d072a9eb979be42c1044e5359371bae2c0a
Julia Pak <jpak0444@gmail.com>
2022-09-25 06:55:53 -0400

    logger

--- lib/globalvars.dart
+++ lib/globalvars.dart
@@ -1,5 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 // for firebase
 final Future<FirebaseApp> initialization = Firebase.initializeApp();
+FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+FirebaseAuth auth = FirebaseAuth.instance;
+
+Logger logger = Logger();
+
+enum WeightUnits { Lb, Oz, Kg }
