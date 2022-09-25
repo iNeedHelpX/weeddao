1db5f9d087435872ca2e842dae4e6cbf79329f52
Julia Pak <jpak0444@gmail.com>
2022-09-25 00:53:16 -0400

    changes

--- lib/Start/bar/bottom_icons.dart
+++ lib/Start/bar/bottom_icons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:rolling_bottom_bar/rolling_bottom_bar_item.dart';
 import 'package:weeddao/icons/resized_joint.dart';
 import 'package:weeddao/icons/resized_sci.dart';
